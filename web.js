2eb162c41d7f5895fe8fa7e510c358e9ade45169
gyrodecl <gyrcl@outlook.com>
2013-07-25 06:24:41 +0000

    Web.js now uses index.html

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var buf = fs.readFileSync('index.html');
+  var buf_as_string = buf.toString;
+
+  response.send(buf_as_string);
 });
 
 var port = process.env.PORT || 5000;
