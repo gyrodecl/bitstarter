7a947a022f3b4202c6ec119f386bc975b56d42d6
gyrodecl <gyrcl@outlook.com>
2013-08-06 06:11:22 +0000

    Updated product description.

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var express = require('express');
-var app = express();
 var fs = require('fs');
+var htmlfile = "index.html";
 
-app.use(express.logger());
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync('index.html');
-  var buf_as_string = buf.toString();
-
-  response.send(buf_as_string);
+  var html = fs.readFileSync(htmlfile).toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
